3083df3aaddeb483b77ed3a4a14bf8499eb2a6ed
User <User@DESKTOP-0DBT0N0>
2020-03-23 20:33:44 +0200

    al 2-lea commit

---
+++ TemaCTS/src/CalitateActivitatiSportive.java
@@ -0,0 +1,14 @@
+
+public class CalitateActivitatiSportive {
+		private char ActivitatiCardio;
+		private char ActivitatiDeForta;
+		
+		
+		public static void cardio() {
+			System.out.println("Scad riscul de deces asociat cu bolile cardiovasculare cu 21%. ");
+		}
+		
+		public static void forta() {
+			System.out.println("Scad riscul de deces asociat cu cancerul cu 31%.");
+		}
+}
--- TemaCTS/src/TestGit.java
+++ TemaCTS/src/TestGit.java
@@ -2,7 +2,12 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Git ! Denumirea proiectului de licen?? este Aplica?ie mobil? pentru monitorizarea activit??ilor sportive ");
+		System.out.println("Hello Git ! Denumirea proiectului de licenta este Aplica?ie mobil? pentru monitorizarea activitatilor sportive ");
+		CalitateActivitatiSportive s = new  CalitateActivitatiSportive(); 
+		s.cardio();
+		s.forta();
 	}
-
+	
+	 
+	 
 }
