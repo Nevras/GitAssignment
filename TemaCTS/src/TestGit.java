ac262d9c637362507e28217d2133693c3947c7c0
User <User@DESKTOP-0DBT0N0>
2020-03-23 19:22:38 +0200

    commit inițial

---
+++ TemaCTS/src/TestGit.java
@@ -0,0 +1,8 @@
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git ! Denumirea proiectului de licen?? este Aplica?ie mobil? pentru monitorizarea activit??ilor sportive ");
+	}
+
+}
